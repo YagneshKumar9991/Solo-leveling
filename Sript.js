b7b23613c37685dac88b74d1ba7f5d8bfc052c47
YagneshKumar9991 <Yagneshkumar.teerupalli@gmail.com>
2025-03-04 17:44:07 +0530

    Add files via upload

---
+++ Sript.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("training-content").innerHTML = "<p>Coming Soon...</p>";
+    document.getElementById("abilities-content").innerHTML = "<p>Unlock and level up your abilities.</p>";
+    document.getElementById("dungeons-content").innerHTML = "<p>Find and conquer dungeons.</p>";
+});
